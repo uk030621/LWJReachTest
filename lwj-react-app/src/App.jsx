56ad57d38ff598d30a4f24072bf10d9f2f5d52d8
uk030621 <123797901+uk030621@users.noreply.github.com>
2024-08-14 21:46:49 +0100

    Defined API Base URL

--- lwj-react-app/src/App.jsx
+++ lwj-react-app/src/App.jsx
@@ -3,13 +3,19 @@ import axios from 'axios';
 import Card from './Card.jsx';
 import pic1 from './assets/Globe.png';
 
+// Define the API Base URL
+const API_BASE_URL = 
+  window.location.hostname === 'localhost'
+  ? 'http://localhost:5000/api/cards'  // Development URL
+  : '/api/cards';  // Production URL
+
+
 
 function App() {
     const [cards, setCards] = useState([]);
 
     useEffect(() => {
-        //axios.get('http://localhost:5000/api/cards')
-        axios.get('/api/cards') //use for internet (vercel) or use previous line for local server. 
+        axios.get(API_BASE_URL) //use for internet (vercel) or use previous line for local server. 
             .then(response => {
                 setCards(response.data);
             })
